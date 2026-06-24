9463e73152e65bb8100a2efb47c3a55c9807b8b9
thecontinium <thecontinium@outlook.com>
2026-06-24 18:07:56 +1000

    ensure node runs in src

--- src/server.js
+++ src/server.js
@@ -3,14 +3,14 @@ import { createAppAuth } from "@octokit/auth-app";
 
 const auth = createAppAuth({
   appId: process.env.GH_APP_ID,
-  privateKey: process.env.GH_APP_PRIVATE_KEY, 
+  privateKey: process.env.GH_APP_PRIVATE_KEY,
 });
 
 createServer(async (req, res) => {
   try {
-    const installationAuth = await auth({ 
-      type: "installation", 
-      installationId: parseInt(process.env.GH_INSTALLATION_ID, 10) 
+    const installationAuth = await auth({
+      type: "installation",
+      installationId: parseInt(process.env.GH_INSTALLATION_ID, 10)
     });
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end(installationAuth.token);
